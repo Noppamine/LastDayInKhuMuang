22ec06981801ff8e092a9c0bad6e972ffc473568
OokaitodoO <m.kanjanawat@gmail.com>
2023-08-10 22:44:16 +0700

    CoolDown dash system

--- LastDayInKhuMuang/Player.cs
+++ LastDayInKhuMuang/Player.cs
@@ -25,10 +25,11 @@ namespace LastDayInKhuMuang
 
         private bool idle = true;
         private bool dashCooldown = false;
-
+        private bool dash = false;
         private bool attack;
 
         private float elapsed;
+        private float temp;
 
 
         private int playerWidth = 30;
@@ -50,54 +51,49 @@ namespace LastDayInKhuMuang
         {
             this.action = action;
         }
+        //Player move
         public void PlayerMove(KeyboardState ks, GraphicsDeviceManager gp, AnimatedTexture animate, GameTime gametime)
         {
             ks = Keyboard.GetState();
             animate.UpdateFrame(elapsed);
-            if (ks.IsKeyDown(Keys.A) && !attack)
-            {             
-                playerPos.X -= speed;
-                idle = false;
-                action = 2;
-            }
-            if (ks.IsKeyDown(Keys.D) && !attack)
+            PlayerDash(ks);
+            if (!dash)
             {
-                playerPos.X += speed;
-                idle = false;
-                action = 3;
+                if (ks.IsKeyDown(Keys.A) && !attack)
+                {
+                    playerPos.X -= speed;
+                    idle = false;
+                    action = 2;
+                }
+                if (ks.IsKeyDown(Keys.D) && !attack)
+                {
+                    playerPos.X += speed;
+                    idle = false;
+                    action = 3;
+                }
+                if (ks.IsKeyDown(Keys.W) && !attack)
+                {
+                    playerPos.Y -= speed;
+                    idle = false;
+                    action = 4;
+                }
+                if (ks.IsKeyDown(Keys.S) && !attack)
+                {
+                    playerPos.Y += speed;
+                    idle = false;
+                    action = 1;
+                }
             }
-            if (ks.IsKeyDown(Keys.W) && !attack)
+            if (dashCooldown)
             {
-                playerPos.Y -= speed;
-                idle = false;
-                action = 4;
+                CoolDown(gametime);
             }
-            if (ks.IsKeyDown(Keys.S) && !attack)
-            {
-                playerPos.Y += speed;
-                idle = false;
-                action = 1;
-            }
+            
             PlayerAttack(ks);
-            //if (ks.IsKeyDown(Keys.J) && action == 2 && !attack) //Left Attack
-            //{
-            //   // attackBox = new Rectangle((int)playerPos.X - playerWidth, (int)playerPos.Y, playerWidth, playerHeight);
-            //    attack = true;
-            //}
-            //if (ks.IsKeyUp(Keys.J))
-            //{
-            //    attack = false;
-            //}
-
             if (ks.IsKeyUp(Keys.A) && ks.IsKeyUp(Keys.D) && ks.IsKeyUp(Keys.W) && ks.IsKeyUp(Keys.S))
             {
                 idle = true;
             }
-            //if (ks.IsKeyUp(Keys.LeftShift))
-            //{
-            //    speed = stdSpeed;
-            //}
-            //Check Collision With Game Screen
             if (playerPos.X > gp.GraphicsDevice.Viewport.Width - playerWidth) 
             {
                 playerPos.X -= speed;
@@ -115,6 +111,34 @@ namespace LastDayInKhuMuang
                 playerPos.Y += speed;
             }
         }
+
+        //Player Dash ability
+        public void PlayerDash(KeyboardState ks)
+        {
+            if (ks.IsKeyDown(Keys.LeftShift) && action == 2 && !dashCooldown && !dash)
+            {
+                playerPos = new Vector2(playerPos.X - 60, playerPos.Y);
+                dash = true;
+                dashCooldown = true;
+            }
+            if (ks.IsKeyUp(Keys.LeftShift))
+            {
+                dash = false;
+            }
+        }
+        public void CoolDown(GameTime gametime)
+        {
+            //temp = 0;
+            temp += (float)gametime.ElapsedGameTime.TotalMilliseconds / 1000;
+            Console.WriteLine(temp);
+            if (temp >= 3)
+            {
+                temp = 0;
+                dashCooldown = false;
+            }
+            
+        }
+
         public void PlayerAttack(KeyboardState ks)
         {
            //KeyboardState oldKs;
